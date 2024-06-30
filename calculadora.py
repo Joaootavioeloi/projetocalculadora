7255d8c9a4f8d23e589e6eeee90c9dd97592412a
Joaootavioeloi <joaootavioeloi@gmail.com>
2024-06-30 18:50:00 -0300

    adicionando o codigo calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+print('Bem vindo programa calculadora')
+#Essa é nossa calculadora e ela é bem simples
+resultado = 1
+print(f"DEBUG: valor da variavel resultado e:{resultado}")
+valor_informado = int(input('informe o seu valor:\n'))
+print(f"DEBUG: valor da variavel iformado e:{valor_informado}")
+resultado = resultado + valor_informado
+print(f"DEBUG: valor da variavel resultado e:{resultado}")
+print(f'seu resultado:{resultado}')
